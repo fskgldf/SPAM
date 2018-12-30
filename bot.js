3e07a365bab50cf64c761bc45bcfe735bdf4a6e4
fskgldf <46267705+fskgldf@users.noreply.github.com>
2018-12-30 22:43:50 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,28 +1,69 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const a = new Discord.Client();
+const b = new Discord.Client();
 console.log("By al7midy");
-
+const ownerid = '525270331611414529';
 
 client.on("ready", () => {
-let channel =     client.channels.get("527067290508722179")
+let channel =     client.channels.get("529016214547070996")
 setInterval(function() {
 channel.send(`1`);
 }, 30)
 })
 
 a.on("ready", () => {
-let channel =     client.channels.get("527067290508722179")
+let channel =     a.channels.get("529016214547070996")
 setInterval(function() {
 channel.send(`2`);
 }, 30)
 })
 
 b.on("ready", () => {
-let channel =     client.channels.get("527067290508722179")
+let channel =     b.channels.get("529016214547070996")
 setInterval(function() {
 channel.send(`3`);
 }, 30)
 })
+
+a.on('message', message => {
+  if (message.author.bot) return;
+  let command = message.content.split(" ")[0];
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "h") {
+        		        if (message.author.id != ownerid) return;
+
+   message.channel.sendMessage(args.join("  "))
+  }
+});
+
+b.on('message', message => {
+  if (message.author.bot) return;
+  let command = message.content.split(" ")[0];
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "h") {
+        		        if (message.author.id != ownerid) return;
+
+   message.channel.sendMessage(args.join("  "))
+  }
+});
+
+client.on('message', message => {
+  if (message.author.bot) return;
+  let command = message.content.split(" ")[0];
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "h") {
+        		        if (message.author.id != ownerid) return;
+
+   message.channel.sendMessage(args.join("  "))
+  }
+});
 
 client.login(process.env.BOT_TOKEN);
 a.login(process.env.BOT_TOKEN1);
